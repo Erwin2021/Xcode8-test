d8b016e5f336bd4e59309213f4c42b3f4cdc0f9f
Erwin Schuurman <e.schuurman@hccnet.nl>
2016-07-26 13:42:45 +0200

    Initial Commit

---
+++ Test Xcode 8 met github/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test Xcode 8 met github
+//
+//  Created by Erwin Schuurman on 26-07-16.
+//  Copyright © 2016 Erwin Schuurman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
